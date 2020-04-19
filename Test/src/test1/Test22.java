b93a02100a4bf30c81f0b6cbe630b948d2c15ab9
clearlove1010 <62588380+clearlove1010@users.noreply.github.com>
2020-04-19 13:20:36 +0800

    Delete Test22.java

--- Test/src/test1/Test22.java
+++
@@ -1,9 +0,0 @@
-package test1;
-
-public class Test11 {
-	public static void main(String[] args) {
-		System.out.println("1233");
-        System.out.println("4566");
-        System.out.println("7899");
-	}
-}
