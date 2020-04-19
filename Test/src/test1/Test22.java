028ae076726e1ea690d27a000816c128f292994b
clearlove1010 <62588380+clearlove1010@users.noreply.github.com>
2020-04-19 13:14:46 +0800

    Create Test22.java

---
+++ Test/src/test1/Test22.java
@@ -0,0 +1,9 @@
+package test1;
+
+public class Test11 {
+	public static void main(String[] args) {
+		System.out.println("1233");
+        System.out.println("4566");
+        System.out.println("7899");
+	}
+}
